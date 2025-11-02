1d55249d1093675f496649182af48a9a5999e939
Dione Castro Alves <84285490+Foxactive1@users.noreply.github.com>
2025-11-02 09:41:27 -0300

    Update Samsung.cs

--- Celulares/Samsung.cs
+++ Celulares/Samsung.cs
@@ -1,1 +1,13 @@
+using System;
+
+namespace POO_Celulares.Celulares
+{
+    public class Samsung : Celular
+    {
+        public Samsung(string modelo) : base("Samsung", modelo) { }
+
+        public override void TirarFoto() => Console.WriteLine($"{Marca} {Modelo}: Tirando foto em Modo Noturno.");
+        public override void EnviarMensagem(string mensagem) => Console.WriteLine($"{Marca} {Modelo}: Enviando via SMS: {mensagem}");
+    }
+}
 
