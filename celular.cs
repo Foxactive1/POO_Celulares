3ed663072e792881fbaf52e54d993ff10aa53aa9
Dione Castro Alves <84285490+Foxactive1@users.noreply.github.com>
2025-11-02 09:43:28 -0300

    Update and rename celular.cs to

--- celular.cs
+++ celular.cs
@@ -1,9 +1,1 @@
-abstract class Celular
-{
-    public string Marca { get; set; }
-    public string Modelo { get; set; }
-    public List<IApp> AppsInstalados { get; set; }
 
-    // Métodos comuns: Ligar, Desligar, InstalarApp, AbrirApp
-    // Métodos abstratos: TirarFoto, EnviarMensagem
-}
